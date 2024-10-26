533ab6775e3cbbb75b25bb9d36c950b8cb8e93f8
Daniel Zhang <danielkzhang11@gmail.com>
2024-10-26 14:08:33 -0400

    Made a very limited skeleton  with '#' will be ignored, and an empty message aborts the commit.

---
+++ app.js
@@ -0,0 +1,11 @@
+function showRegister() {
+    window.location.href = "register.html";
+}
+
+function showLogin() {
+    window.location.href = "login.html";
+}
+
+function showDashboard() {
+    window.location.href = "dashboard.html";
+}
